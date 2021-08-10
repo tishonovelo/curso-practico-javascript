024710ca47c24cc4e55cad58df160a1b2d1f61c8
Patricio Alberto Novelo Novelo <patricionovelo@MacBook-Air-de-Patricio.local>
2021-08-10 13:40:15 -0500

    Añadí el index.html con links al resto de documentos

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # curso-practico-javascript
 Hola este es mi primer commit
+
+...
+
+## Taller #1: figuras geométricas
+
+- Primer paso: definir las fórmulas
+- Segundo paso: implementar las fórmulas en JavaScript
+- Tercer paso: crear funciones
+- Cuarto paso: integrar JS con HTML
+
+## Taller #2: porcentajes y descuentos
+
+- Primer paso: definir las fórmulas
+- Segundo paso: implementar las fórmulas en JavaScript
+- Tercer paso: crear funciones
+- Cuarto paso: integrar JS con HTML
+
+## Taller #2: promedio, mediana y moda
+
+- Primer paso: definir las fórmulas
+- Segundo paso: implementar las fórmulas en JavaScript
+- Tercer paso: crear funciones
+- Cuarto paso: integrar JS con HTML
---
+++ salarios.js
@@ -0,0 +1,81 @@
+const colombia = [];
+colombia.push({
+    name: "Juanita",
+    salary: 1000,
+});
+colombia.push({
+    name: "Natalia",
+    salary: 1200,
+});
+colombia.push({
+    name: "Laura",
+    salary: 2100,
+});
+colombia.push({
+    name: "Mariel",
+    salary: 3000,
+});
+colombia.push({
+    name: "Fernanda",
+    salary: 900,
+});
+colombia.push({
+    name: "Andrea",
+    salary: 1100,
+});
+colombia.push({
+    name: "Mariana",
+    salary: 1200,
+});
+colombia.push({
+    name: "Melissa",
+    salary: 750,
+});
+colombia.push({
+    name: "Maricruz",
+    salary: 1300,
+});
+colombia.push({
+    name: "Claudia",
+    salary: 2200,
+});
+colombia.push({
+    name: "Naomi",
+    salary: 2500,
+});
+colombia.push({
+    name: "Alejandra",
+    salary: 800,
+});
+colombia.push({
+    name: "Roberta",
+    salary: 850,
+});
+colombia.push({
+    name: "Ingrid",
+    salary: 1700,
+});
+colombia.push({
+    name: "Jessica",
+    salary: 2000,
+});
+colombia.push({
+    name: "Alexa",
+    salary: 1900,
+});
+colombia.push({
+    name: "Maricruz",
+    salary: 1000,
+});
+colombia.push({
+    name: "Juliana",
+    salary: 1400,
+});
+colombia.push({
+    name: "Julia",
+    salary: 2700,
+});
+colombia.push({
+    name: "Maria",
+    salary: 900,
+});
